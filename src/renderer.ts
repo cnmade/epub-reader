a8b9416387a53d7ad6bfaf33ef1eb7d91a5face6
ysdkfmcxkfeiw <sdkfjl@localhost>
2021-05-09 17:25:02 +0800

    修订软件代码

--- src/renderer.ts
+++ src/renderer.ts
@@ -38,32 +38,32 @@ import { app, ipcRenderer } from 'electron';
 jQuery.noConflict();
 
 function getSearchParameters() {
-  var prmstr = window.location.search.substr(1);
+  let prmstr = window.location.search.substr(1);
   return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : new Map();
 }
 console.log('rawUrl:' + JSON.stringify(window.location));
 
 function transformToAssocArray(prmstr: string) {
-  var params = new Map();
-  var prmarr = prmstr.split("&");
-  for (var i = 0; i < prmarr.length; i++) {
-    var tmparr = prmarr[i].split("=");
+  let params = new Map();
+  let prmarr = prmstr.split("&");
+  for (let i = 0; i < prmarr.length; i++) {
+    let tmparr = prmarr[i].split("=");
     params.set(tmparr[0], tmparr[1]);
   }
   return params;
 }
 
-var params = getSearchParameters();
+let params = getSearchParameters();
 
 
-var sm = params.get("args");
+let sm = params.get("args");
 console.log("args:" + JSON.stringify(sm));
 
 if (sm == "" || sm == ".") {
   //dev
-  sm = "file:///./rust.epub";
+ // sm = "file:///./rust.epub";
   //prod
-  // sm = "";
+   sm = "";
 }
 
 
@@ -75,8 +75,8 @@ if (sm == "") {
   window.alert("请打开一个epub文件");
   ipcRenderer.send('close-me');
 } else {
-  var book = ePub(sm);
-  var rendition = book.renderTo("viewer", {
+  let book = ePub(sm);
+  let rendition = book.renderTo("viewer", {
     width: "100%",
     height: 800,
     spread: "always"
@@ -88,7 +88,7 @@ if (sm == "") {
 
   //菜单
 
-  var tocDiv = document.getElementById("catalog");
+  let tocDiv = document.getElementById("catalog");
   tocDiv.addEventListener("mouseenter", () => {
     jQuery("#catalog").width("300").height("800").css("overflow", "auto");
 
@@ -102,20 +102,20 @@ if (sm == "") {
 
     window.addEventListener("resize", () => rendition.resize(window.innerWidth, window.innerHeight));
 
-    var next = document.getElementById("next");
+    let next = document.getElementById("next");
 
     next.addEventListener("click", function (e) {
       rendition.next();
       e.preventDefault();
     }, false);
 
-    var prev = document.getElementById("prev");
+    let prev = document.getElementById("prev");
     prev.addEventListener("click", function (e) {
       rendition.prev();
       e.preventDefault();
     }, false);
 
-    var keyListener = function (e: { keyCode: any; which: any; }) {
+    let keyListener = function (e: { keyCode: any; which: any; }) {
 
       // Left Key
       if ((e.keyCode || e.which) == 37) {
@@ -134,11 +134,11 @@ if (sm == "") {
 
   })
 
-  var title = document.getElementById("title");
+  let title = document.getElementById("title");
 
 
 
-  var recursionHandle = function (toc: NavItem[], doc: string[], i: number) {
+  let recursionHandle = function (toc: NavItem[], doc: string[], i: number) {
     toc.forEach(function (chapter: NavItem) {
       doc.push('<p class="catalog-item catalog-item-' + i + '" data-catalog="' + chapter.href + '">' + chapter.label + '</p>')
       if (chapter.subitems && chapter.subitems.length) {
@@ -152,12 +152,12 @@ if (sm == "") {
   }
 
   rendition.on("rendered", function (section: { href: string; }) {
-    var current = book.navigation && book.navigation.get(section.href);
+    let current = book.navigation && book.navigation.get(section.href);
     book.loaded.navigation.then(function (toc) {
 
       // 方式一 toc是一个多维数组，下面这种只能显示第一级的目录
 
-      var catalogitem = '';
+      let catalogitem = '';
       toc.forEach(function (chapter: NavItem) {
         catalogitem += '<p class="catalog-item" data-catalog="' + chapter.href + '">' + chapter.label + '</p>';
         return "";
@@ -174,7 +174,7 @@ if (sm == "") {
       // 点击跳转
       jQuery('.catalog-item').on('click', function () {
         // 当点击报错的时候，请看下面  杂项-目录跳转报错
-        var url = jQuery(this).attr("data-catalog");
+        let url = jQuery(this).attr("data-catalog");
         console.log(url);
         rendition.display(url);
       });
@@ -189,8 +189,8 @@ if (sm == "") {
   rendition.on("relocated", function (location: { atEnd: any; atStart: any; }) {
     console.log(location);
 
-    var next = document.getElementById("next");
-    var prev = document.getElementById("prev");
+    let next = document.getElementById("next");
+    let prev = document.getElementById("prev");
 
     if (location.atEnd) {
       next.style.visibility = "hidden";
